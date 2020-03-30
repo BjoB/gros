6b91c58999fa540600184ee2c7d1476cb2fb448b
Bjob <bjoern.barschtipan@gmail.com>
2020-03-30 16:36:43 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gros',
+    version='0.1.0',
+    description='General Relativity Orbit Simulator',
+    url='https://github.com/BjoB/gros',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Björn Barschtipan',
+    author_email='',
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    setup_requires=['wheel'],
+    python_requires='>=3.5',
+)
