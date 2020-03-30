71aa946f745dc1e95f5526d05b55d885a423cc50
Bjob <bjoern.barschtipan@gmail.com>
2020-03-30 17:04:02 +0200

    Refactor simple test for Schwarschild geodesic

--- src/gros/tests/metric/test_schwarzschild.py
+++ src/gros/tests/metric/test_schwarzschild.py
@@ -1,12 +1,23 @@
-from gros.metric import SchwarzschildMetric
+from gros.metric import schwarzschild as ss
 from gros.utils import const
+from numpy import pi
+from numpy import sqrt
 
 
 def calc_schwarzschild_trajectory():
-    ss_metric = SchwarzschildMetric(
+    r = 3400
+
+    ss_metric = ss.SchwarzschildMetric(
         M=const.M_solar,
-        initial_vec_pos=[1e6, 0, 0],
-        initial_vec_v=[1e3, 0.001, 0]
+        initial_vec_pos=[r, pi/2, 0.3],
+        initial_vec_v=[0.0, 0.0, 0.0]
     )
 
-    ss_metric.calc_trajectory_iterator()
+    for _, val in zip(range(5000), ss_metric.generate_trajectory()):
+        print("tau = {}s, t = {}s, r = {}m, dt/dtau = {}".format(val[0], val[1][0], val[1][1], val[1][4]))
+
+    dt_factor = 1/sqrt(1 - ss.calc_schwarzschild_radius(const.M_solar).value / r)
+    print("theoretical dt/dtau = {}".format(dt_factor))
+
+
+calc_schwarzschild_trajectory()
