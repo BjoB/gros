5352de1a67d733709b8824e650bf73e739b64ea4
Bjob <bjoern.barschtipan@gmail.com>
2020-04-01 01:20:52 +0200

    Update test case for trajectory

--- src/gros/tests/metric/test_schwarzschild.py
+++ src/gros/tests/metric/test_schwarzschild.py
@@ -5,15 +5,15 @@ from numpy import sqrt
 
 
 def calc_schwarzschild_trajectory():
-    r = 3400
+    r = 10000000
 
     ss_metric = ss.SchwarzschildMetric(
         M=const.M_solar,
-        initial_vec_pos=[r, pi/2, 0.3],
-        initial_vec_v=[0.0, 0.0, 0.0]
+        initial_vec_pos=[r, pi/2, 0.0],
+        initial_vec_v=[1000, 0.0, 0.088]
     )
 
-    for _, val in zip(range(5000), ss_metric.generate_trajectory()):
+    for _, val in zip(range(100), ss_metric.generate_trajectory()):
         print("tau = {}s, t = {}s, r = {}m, dt/dtau = {}".format(val[0], val[1][0], val[1][1], val[1][4]))
 
     dt_factor = 1/sqrt(1 - ss.calc_schwarzschild_radius(const.M_solar).value / r)
