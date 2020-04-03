66e6882a3d87a81b9b0a325fce14800859e11751
Bjob <bjoern.barschtipan@gmail.com>
2020-04-03 11:19:24 +0200

    Add mercury orbit parameters to example

--- src/gros/examples/simple_trajectory.py
+++ src/gros/examples/simple_trajectory.py
@@ -9,23 +9,28 @@ from gros.utils import const
 def simple_trajectory():
     # Initialize metric
 
-    # ss_metric = ss.SchwarzschildMetric(
-    #     M=const.M_solar,
-    #     initial_vec_pos=[46e6 * 1000, pi/2, 0],
-    #     initial_vec_v=[0, 0, (47362 / 46e6)]
-    # )
-
-    # TODO: move initial vecs into calculate_trajectory method
-    # TODO: put proper time, coordinate time to plot and animation
-    # TODO: faster animation with frames!
     ss_metric = ss.SchwarzschildMetric(
-        M=const.M_earth,
-        initial_vec_pos=[140, pi/2, 0],
-        initial_vec_v=[0, 0, 1800]
+        M=const.M_solar,
+        initial_vec_pos=[57.9e6 * 1000, pi/2, 0],
+        initial_vec_v=[0, 0, (47.4 / 57.9e6)]
     )
 
-    traj = ss_metric.calculate_trajectory(proptime_end=0.002, step_size=5e-7)
-    traj.plot()
+    # TODO:
+    # move initial vecs into calculate_trajectory method
+    # add optional object radius for plot (add to metric initialization via "grav. center object")
+    # put proper time, coordinate time to plot and animation
+    # document plotting methods
+    # faster animation with frames!
+    # add planet / black hole data to const
+
+    # ss_metric = ss.SchwarzschildMetric(
+    #     M=const.M_earth,
+    #     initial_vec_pos=[140, pi/2, 0],
+    #     initial_vec_v=[0, 0, 1800]
+    # )
+
+    traj = ss_metric.calculate_trajectory(proptime_end=88 * 24 * 3600, step_size=1000)
+    traj.plot(attractor_radius=6.9634e8)
 
 
 if __name__ == "__main__":
