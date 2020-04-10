f7640591bf6fecf43a18164ffdabd452dd34fc95
Bjob <bjoern.barschtipan@gmail.com>
2020-04-10 13:54:23 +0200

    Update earth black hole example for simulation

--- src/gros/examples/earth_black_hole_trajectory.py
+++ src/gros/examples/earth_black_hole_trajectory.py
@@ -9,12 +9,12 @@ from gros.utils import const
 def earth_black_hole_trajectory():
     ss_metric = ss.SchwarzschildMetric(
         M=const.M_earth,
-        initial_vec_pos=[100, pi/2, 0],
-        initial_vec_v=[0, 0, 5000]
+        initial_vec_pos=[30, pi/2, 0],
+        initial_vec_v=[0, 0, 20000]
     )
 
-    traj = ss_metric.calculate_trajectory(proptime_end=0.005, step_size=1e-5)
-    traj.plot()
+    traj = ss_metric.calculate_trajectory(proptime_end=1.2e-4, step_size=1e-6)
+    traj.plot(animation_step_size=1e-6)
 
 
 if __name__ == "__main__":
